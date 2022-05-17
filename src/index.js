c0852188e429ccf9743614ffa2ce2f6fa7ecdba3
ZeldOcarina <mattia.r86@gmail.com>
2022-05-17 10:35:41 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import * as bootstrap from "bootstrap";
+
+var collapseElementList = [].slice.call(document.querySelectorAll(".collapse"));
+var collapseList = collapseElementList.map(function (collapseEl) {
+  return new bootstrap.Collapse(collapseEl);
+});
