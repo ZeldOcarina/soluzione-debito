69e042cd744ba90df642fe6da0e7a613f84d5738
roman3672 <romka.3672@gmail.com>
2022-05-19 03:15:54 +0300

    desktop layout

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ var collapseElementList = [].slice.call(document.querySelectorAll(".collapse"));
 var collapseList = collapseElementList.map(function (collapseEl) {
   return new bootstrap.Collapse(collapseEl);
 });
+
+document.getElementById("date").innerText = new Date().getFullYear();
